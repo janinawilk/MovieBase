5ef2c913cdbab7ab85163f624044c0026349615a
twinie96 <twinie96@gmail.com>
2018-07-10 10:18:45 +0200

    Add friendly_id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -56,7 +56,7 @@ class MoviesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
-      @movie = Movie.find(params[:id])
+      @movie = Movie.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   has_one_attached :picture
+
+  include FriendlyId
+  friendly_id :title, use: :slugged
+
 end
